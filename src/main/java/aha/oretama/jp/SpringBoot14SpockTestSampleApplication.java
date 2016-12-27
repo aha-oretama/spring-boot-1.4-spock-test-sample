4c26e32557bf95bf566fb3529e70c9bd9fb0202e
yasufumi sekine <yasufumi_sekine@r.recruit.co.jp>
2016-12-27 18:12:14 +0900

    Initial Commit

---
+++ src/main/java/aha/oretama/jp/SpringBoot14SpockTestSampleApplication.java
@@ -0,0 +1,12 @@
+package aha.oretama.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot14SpockTestSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot14SpockTestSampleApplication.class, args);
+	}
+}
